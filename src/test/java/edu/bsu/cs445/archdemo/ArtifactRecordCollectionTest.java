44554b7adc614ed00628fb078666b69e9e608463
EnergizedBlast <energize98@gmail.com>
2019-02-18 21:20:02 -0500

    Fix warning from ArtifactRecordCollection

--- src/test/java/edu/bsu/cs445/archdemo/ArtifactRecordCollectionTest.java
+++ src/test/java/edu/bsu/cs445/archdemo/ArtifactRecordCollectionTest.java
@@ -1,15 +1,12 @@
 package edu.bsu.cs445.archdemo;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashSet;
 import java.util.List;
 
 class ArtifactRecordCollectionTest {
 
-    @DisplayName("Test that countRecordByTitleQuery returns 0 records if query-term is empty")
     @Test
     void testCountRecordsByTitleQuery_emptyString_zero() {
         ArtifactRecordCollection collection = ArtifactRecordCollection.createEmpty();
@@ -19,7 +16,6 @@ class ArtifactRecordCollectionTest {
         Assertions.assertEquals(0, countExactTrue);
     }
 
-    @DisplayName("Test that countRecordByTitleQuery returns 1 record if query-term is in collection")
     @Test
     void testCountRecordsByTitleQuery_oneItemMatch_one() {
         ArtifactRecord record = ArtifactRecord.withTitle("Foo");
@@ -30,7 +26,6 @@ class ArtifactRecordCollectionTest {
         Assertions.assertEquals(1, countExactTrue);
     }
 
-    @DisplayName("Test that countRecordByTitleQuery returns 0 records if query-term is not in collection")
     @Test
     void testCountRecordsByTitleQuery_noItemsMatchInNonemptyCollection_zero() {
         ArtifactRecord record = ArtifactRecord.withTitle("Foo");
@@ -41,7 +36,6 @@ class ArtifactRecordCollectionTest {
         Assertions.assertEquals(0, countExactTrue);
     }
 
-    @DisplayName("Test that countRecordByTitleQuery returns 2 records if the query-term is seen in twice in collection")
     @Test
     void testCountRecordsByTitleQuery_twoItemsMatch_two() {
         ArtifactRecordCollection collection = ArtifactRecordCollection.of(
@@ -51,7 +45,6 @@ class ArtifactRecordCollectionTest {
         Assertions.assertEquals(2, countExactFalse);
     }
 
-    @DisplayName("Test that the function searchTitles does not return results for query-term that is not in collection")
     @Test
     void testSearchTitles() {
         final String testTitle = "Foo";
@@ -64,20 +57,21 @@ class ArtifactRecordCollectionTest {
         Assertions.assertEquals(testTitle, resultExactTrue.get(0).getTitle());
     }
 
-    @DisplayName("Test that the function searchSubject does not return results for query-term that is not in collection")
     @Test
     void testSearchSubject(){
-        final String testSubject = "Foo";
+        final String testSubject1 = "Foo";
+        final String testSubject2 = "Bar";
         final HashSet<String> testSubjectList = new HashSet<>();
-        testSubjectList.add(testSubject);
+        testSubjectList.add(testSubject1);
+        testSubjectList.add(testSubject2);
         ArtifactRecordCollection collection = ArtifactRecordCollection.of(
-                ArtifactRecord.withSubject(testSubject)
+                ArtifactRecord.withSubject(testSubject1 + " ; " + testSubject2)
         );
         List<ArtifactRecord> resultExactFalse = collection.searchSubject(testSubjectList, false);
-        Assertions.assertEquals(testSubject, resultExactFalse.get(0).getSubject_LCSH());
+        Assertions.assertEquals(testSubject1 + " ; " + testSubject2, resultExactFalse.get(0).getSubject_LCSH());
 
         List<ArtifactRecord>resultExactTrue = collection.searchSubject(testSubjectList, true);
-        Assertions.assertEquals(testSubject, resultExactTrue.get(0).getSubject_LCSH());
+        Assertions.assertEquals(testSubject1 + " ; " + testSubject2, resultExactTrue.get(0).getSubject_LCSH());
 
     }
 }

ac2aeae2c1c123f7be9293ce81b899723726ece2
EnergizedBlast <energize98@gmail.com>
2019-04-04 16:29:25 -0400

    Add SearchEngine class

---
+++ src/main/java/edu/bsu/cs445/archdemo/SearchEngine.java
@@ -0,0 +1,30 @@
+package edu.bsu.cs445.archdemo;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class SearchEngine {
+
+    private List<ArtifactRecord> artifacts;
+    private List<ArtifactRecord> result;
+
+    SearchEngine(ArtifactRecordCollection collection) {
+        this.artifacts = collection.items;
+    }
+
+    List<ArtifactRecord> searchTitles_WholeWord(String query) {
+        result = artifacts.stream()
+                .filter(artifactRecord -> artifactRecord.getTitle().toLowerCase()
+                .matches(".*\\b" + query.toLowerCase() + "\\b.*")).collect(Collectors.toList());
+        return ImmutableList.copyOf(result);
+    }
+
+    List<ArtifactRecord> searchTitles_Contains(String query) {
+        result = artifacts.stream()
+                .filter(artifactRecord -> artifactRecord.getTitle().toLowerCase()
+                .contains(query.toLowerCase())).collect(Collectors.toList());
+        return ImmutableList.copyOf(result);
+    }
+
+}

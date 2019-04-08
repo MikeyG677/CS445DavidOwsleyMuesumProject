9e027cc95126d07eade08a43c70171b376d8ef0b
aluees <rcklueber@bsu.edu>
2019-04-08 13:48:47 -0400

    Further work on DomaArtifactRecordCollection. Records are now actually added to the DomaArtifactRecordCollection, and loops have been restructured to properly parse.

--- src/main/java/edu/bsu/cs445/archdemo/DomaArtifactRecordCollection.java
+++ src/main/java/edu/bsu/cs445/archdemo/DomaArtifactRecordCollection.java
@@ -1,6 +1,10 @@
 package edu.bsu.cs445.archdemo;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.util.List;
 
 class DomaArtifactRecordCollection {
 
@@ -9,25 +13,34 @@ class DomaArtifactRecordCollection {
         DomaArtifactRecordCollection collection = new DomaArtifactRecordCollection();
         for(int i=0; i<jaxbCollection.size(); i++){
             ArtifactRecord record = jaxbCollection.items.get(i);
-            if(record.getFileName().contains(".cpd")) { //record.getFilename().contains(".cpd")
+            if(record.getFileName().contains(".cpd")) {
                 int j = 1;
+                collection.records.add(record);
                 while(true){
                     ArtifactRecord recordAbove = jaxbCollection.items.get(i-j);
-                    //Grab image, delete record.
-                    if(recordAbove.getTitle().isEmpty()){
+                    if(!recordAbove.getTitle().isEmpty()){
                         break;
                     }
+                    //Grab image and add it to .cpds images. Do NOT add the record.
+
                     j += 1;
                 }
                 System.out.println("This is a .cpd!");
             }
-            else if(record.getTitle().isEmpty()){
-                System.out.println("This is an image for a .cpd");
+            else if(!record.getTitle().isEmpty()){
+                System.out.println("Record.");
+                collection.records.add(record);
             }
             else{
-                System.out.println("No.");
+                System.out.println("This is an image for a .cpd.");
             }
         }
         return collection;
     }
+
+    @SuppressWarnings({"unused","MismatchedQueryAndUpdateOfCollection"})
+    @XmlElement(name="DOMA records")
+    private List<ArtifactRecord> records = Lists.newArrayList();
+
+    int size() { return records.size(); }
 }

a8148ee9d1297b5d21e1f9aef13b4aa7f57de8ff
Robert Klueber <rcklueber@bsu.edu>
2019-04-08 18:53:30 -0400

    Refactored/cleaned up Main.java.

--- src/main/java/edu/bsu/cs445/archdemo/Main.java
+++ src/main/java/edu/bsu/cs445/archdemo/Main.java
@@ -1,20 +1,14 @@
 package edu.bsu.cs445.archdemo;
-import com.google.common.base.Preconditions;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 
 public class Main extends Application {
 
     private JaxbArtifactRecordCollection jaxbCollection;
-    private DomaArtifactRecordCollection domaCollection;
 
     @Override
     public void start(Stage primaryStage) {
@@ -25,9 +19,7 @@ public class Main extends Application {
         InputStream owsleyStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("owsley.xml");
         CompletableFuture.runAsync(() -> jaxbCollection = parser.parse(owsleyStream))
                 .thenRun(() -> Platform.runLater(() -> {
-                            System.out.println(jaxbCollection.size());
                             DomaArtifactRecordCollection convertedCollection = domaCollection.convertJaxbToDoma(jaxbCollection);
-                            System.out.println(convertedCollection.records.toString());
                             SearchPane searchPane = new SearchPane(convertedCollection);
                             Scene searchPaneScene = new Scene(searchPane);
                             primaryStage.setScene(searchPaneScene);

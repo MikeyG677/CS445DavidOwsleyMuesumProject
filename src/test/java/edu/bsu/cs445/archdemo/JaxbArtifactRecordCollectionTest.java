6330732263466408127e7f7b81203e2cdd90a04c
EnergizedBlast <energize98@gmail.com>
2019-04-09 00:20:35 -0400

    Restructure JaxbArtifactRecordCollectionTest; provide 100% test coverage for JaxbArtifactRecordCollection

--- src/test/java/edu/bsu/cs445/archdemo/JaxbArtifactRecordCollectionTest.java
+++ src/test/java/edu/bsu/cs445/archdemo/JaxbArtifactRecordCollectionTest.java
@@ -2,160 +2,32 @@ package edu.bsu.cs445.archdemo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 
 class JaxbArtifactRecordCollectionTest {
-    @Test
-    void testResultCount_zero(){
-        //WIP and getting done as you read this!
+    ArtifactRecord buildTestRecord() {
+        ArtifactRecord testRecord = new ArtifactRecord();
+
+        testRecord.title = "Foo";
+        testRecord.fileName = "Bar";
+        testRecord.artist = "Fool";
+        testRecord.subject_LCSH = "Foo ; Bar";
+        testRecord.date_Made = "1969";
+        testRecord.culture = "American";
+        testRecord.century = "21st";
+        testRecord.periodStyle = "Cubism";
+
+        return testRecord;
     }
 
-
-
-/*///////////////////////////////////////////////////////////////////////////////
     @Test
-    void testCountRecordsByTitleQuery_emptyString_zero() {
+    void testSize_zero() {
         JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.createEmpty();
-        int countExactFalse = collection.countRecordsByTitleQuery("", false);
-        Assertions.assertEquals(0, countExactFalse);
-        int countExactTrue = collection.countRecordsByTitleQuery("", true);
-        Assertions.assertEquals(0, countExactTrue);
+        Assertions.assertEquals(0, collection.size());
     }
 
     @Test
-    void testCountRecordsByTitleQuery_oneItemMatch_one() {
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "Foo";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Foo");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord record = ArtifactRecord.withSpecificInfo(testInfo);
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(record);
-        int countExactFalse = collection.countRecordsByTitleQuery("Foo", false);
-        Assertions.assertEquals(1, countExactFalse);
-        int countExactTrue = collection.countRecordsByTitleQuery("Foo", true);
-        Assertions.assertEquals(1, countExactTrue);
+    void testCollection_one() {
+        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(buildTestRecord());
+        Assertions.assertEquals(1, collection.size());
     }
-
-    @Test
-    void testCountRecordsByTitleQuery_noItemsMatchInNonemptyCollection_zero() {
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "Foo";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Foo");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord record = ArtifactRecord.withSpecificInfo(testInfo);
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(record);
-        int countExactFalse = collection.countRecordsByTitleQuery("Bar", false);
-        Assertions.assertEquals(0, countExactFalse);
-        int countExactTrue = collection.countRecordsByTitleQuery("Bar", true);
-        Assertions.assertEquals(0, countExactTrue);
-    }
-
-    @Test
-    void testCountRecordsByTitleQuery_twoItemsMatch_two() {
-        final HashMap<String, String> testInfo1 = new HashMap<>();
-        final String testTitle1 = "Foo";
-        testInfo1.put("title", testTitle1);
-        testInfo1.put("fileName", "Foo");
-        testInfo1.put("artist", "Bar");
-        testInfo1.put("subject_LCSH", "Fool");
-        testInfo1.put("date_Made", "Barl");
-
-        final HashMap<String, String> testInfo2 = new HashMap<>();
-        final String testTitle2 = "Fool";
-        testInfo2.put("title", testTitle2);
-        testInfo2.put("fileName", "Foo");
-        testInfo2.put("artist", "Bar");
-        testInfo2.put("subject_LCSH", "Fool");
-        testInfo2.put("date_Made", "Barl");
-
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(
-                ArtifactRecord.withSpecificInfo(testInfo1),
-                ArtifactRecord.withSpecificInfo(testInfo2));
-        int countExactFalse = collection.countRecordsByTitleQuery("Foo", false);
-        Assertions.assertEquals(2, countExactFalse);
-    }
-
-    @Test
-    void testSearchTitles() {
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "Foo";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Foo");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(
-                ArtifactRecord.withSpecificInfo(testInfo)
-        );
-        List<ArtifactRecord> resultExactFalse = collection.searchThroughTitles(testTitle, false);
-        Assertions.assertEquals(testTitle, resultExactFalse.get(0).getTitle());
-        List<ArtifactRecord>resultExactTrue = collection.searchThroughTitles(testTitle, true);
-        Assertions.assertEquals(testTitle, resultExactTrue.get(0).getTitle());
-    }
-
-    @Test
-    void testCountRecordsBySubjectQuery_emptyString_zero() {
-        final HashSet<String> query = new HashSet<>();
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.createEmpty();
-        int countExactFalse = collection.countRecordsBySubjectQuery(query, false);
-        Assertions.assertEquals(0, countExactFalse);
-        int countExactTrue = collection.countRecordsBySubjectQuery(query, true);
-        Assertions.assertEquals(0, countExactTrue);
-    }
-
-    @Test
-    void testCountRecordsBySubjectQuery_oneItemMatch_one() {
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testSubject = "Foo";
-        final HashSet<String> testQuery = new HashSet<>();
-        testQuery.add(testSubject);
-        testInfo.put("title", "Foo");
-        testInfo.put("fileName", "Bar");
-        testInfo.put("artist", "Fool");
-        testInfo.put("subject_LCSH", testSubject);
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord record = ArtifactRecord.withSpecificInfo(testInfo);
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(record);
-        int countExactFalse = collection.countRecordsBySubjectQuery(testQuery, false);
-        Assertions.assertEquals(1, countExactFalse);
-        int countExactTrue = collection.countRecordsBySubjectQuery(testQuery, true);
-        Assertions.assertEquals(1, countExactTrue);
-    }
-
-    @Test
-    void testSearchSubjectEXPERIMENTAL(){
-        final HashSet<String> testSubjectList = new HashSet<>();
-        testSubjectList.add("Foo");
-        testSubjectList.add("Bar");
-        final HashMap<String, String> testInfo = new HashMap<>();
-        String testSubjects = "Foo ; Bar";
-        testInfo.put("title", "Foo");
-        testInfo.put("fileName", "Bar");
-        testInfo.put("artist", "Fool");
-        testInfo.put("subject_LCSH", testSubjects);
-        testInfo.put("date_Made", "Barl");
-
-        JaxbArtifactRecordCollection collection = JaxbArtifactRecordCollection.of(
-                ArtifactRecord.withSpecificInfo(testInfo)
-        );
-        List<ArtifactRecord> resultExactFalse = collection.searchThroughSubjects(testSubjectList, false);
-        Assertions.assertEquals(testSubjects, resultExactFalse.get(0).getSubject_LCSH());
-        List<ArtifactRecord>resultExactTrue = collection.searchThroughSubjects(testSubjectList, true);
-        Assertions.assertEquals(testSubjects, resultExactTrue.get(0).getSubject_LCSH());
-
-    }
-}
-*/
 }

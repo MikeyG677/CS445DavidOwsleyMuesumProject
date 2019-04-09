83ff721972b7189a3142f9e220b9d1ebaeee50e4
EnergizedBlast <energize98@gmail.com>
2019-04-09 08:54:16 -0400

    Add 100% test coverage for SearchEngine

--- src/test/java/edu/bsu/cs445/archdemo/SearchEngineTest.java
+++ src/test/java/edu/bsu/cs445/archdemo/SearchEngineTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SearchEngineTest {
+
+    private DomaArtifactRecordCollection testCollection_empty = new DomaArtifactRecordCollection();
+
     @Test
     void testSearchTitle_WholeWord_zeroResults(){
-        //DomaArtifactRecordCollection
-        //SearchEngine testEngine = new SearchEngine();
+        SearchEngine testEngine = new SearchEngine(testCollection_empty);
+        Assertions.assertEquals(0, testEngine.searchTitle_WholeWord("query").size());
+    }
+
+    @Test
+    void testSearchTitle_Contains_zeroResults(){
+        SearchEngine testEngine = new SearchEngine(testCollection_empty);
+        Assertions.assertEquals(0, testEngine.searchTitle_Contains("query").size());
     }
 }

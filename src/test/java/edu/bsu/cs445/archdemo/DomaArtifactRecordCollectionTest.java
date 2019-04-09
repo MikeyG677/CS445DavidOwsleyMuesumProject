685d07423856cba7fade78cef6987fd02dc33183
Robert Klueber <rcklueber@bsu.edu>
2019-04-08 23:41:04 -0400

    Changed DomaArtifactRecordCollectionTest, in light of changes to ArtifactRecord.

--- src/test/java/edu/bsu/cs445/archdemo/DomaArtifactRecordCollectionTest.java
+++ src/test/java/edu/bsu/cs445/archdemo/DomaArtifactRecordCollectionTest.java
@@ -3,9 +3,52 @@ package edu.bsu.cs445.archdemo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-
 class DomaArtifactRecordCollectionTest {
+
+    private ArtifactRecord buildTestRecordJpg(){
+        ArtifactRecord testRecord = new ArtifactRecord();
+
+        testRecord.title = "Foo";
+        testRecord.fileName = "Bar.jpg";
+        testRecord.artist = "Fool";
+        testRecord.subject_LCSH = "Foo ; Bar";
+        testRecord.date_Made = "1969";
+        testRecord.culture = "American";
+        testRecord.century = "21st";
+        testRecord.periodStyle = "Cubism";
+
+        return testRecord;
+    }
+
+    private ArtifactRecord buildTestRecordCpd(){
+        ArtifactRecord testRecord = new ArtifactRecord();
+
+        testRecord.title = "Foo";
+        testRecord.fileName = "Bar.cpd";
+        testRecord.artist = "Fool";
+        testRecord.subject_LCSH = "Foo ; Bar";
+        testRecord.date_Made = "1969";
+        testRecord.culture = "American";
+        testRecord.century = "21st";
+        testRecord.periodStyle = "Cubism";
+
+        return testRecord;
+    }
+
+    private ArtifactRecord buildTestRecordNoTitle(){
+        ArtifactRecord testRecord = new ArtifactRecord();
+
+        testRecord.title = "";
+        testRecord.fileName = "Bar.jpg";
+        testRecord.artist = "Fool";
+        testRecord.subject_LCSH = "Foo ; Bar";
+        testRecord.date_Made = "1969";
+        testRecord.culture = "American";
+        testRecord.century = "21st";
+        testRecord.periodStyle = "Cubism";
+
+        return testRecord;
+    }
 
     @Test
     void testCountRecords_emptyCollection(){
@@ -15,32 +58,16 @@ class DomaArtifactRecordCollectionTest {
 
     @Test
     void testCountRecords_collection_three(){
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "Foo";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Foo.jpg");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord record1 = ArtifactRecord.withSpecificInfo(testInfo);
-        ArtifactRecord record2 = ArtifactRecord.withSpecificInfo(testInfo);
+        ArtifactRecord record1 = buildTestRecordJpg();
+        ArtifactRecord record2 = buildTestRecordJpg();
         DomaArtifactRecordCollection collection = DomaArtifactRecordCollection.of(record1, record2);
         Assertions.assertEquals(2, collection.size());
     }
 
     @Test
     void testConvertJaxbToDoma_noCpd(){
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "Foo";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Foo.jpg");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord record1 = ArtifactRecord.withSpecificInfo(testInfo);
-        ArtifactRecord record2 = ArtifactRecord.withSpecificInfo(testInfo);
+        ArtifactRecord record1 = buildTestRecordJpg();
+        ArtifactRecord record2 = buildTestRecordJpg();
         JaxbArtifactRecordCollection jaxbCollection = JaxbArtifactRecordCollection.of(record1, record2);
         DomaArtifactRecordCollection collection = new DomaArtifactRecordCollection();
         DomaArtifactRecordCollection convertedCollection = collection.convertJaxbToDoma(jaxbCollection);
@@ -49,33 +76,9 @@ class DomaArtifactRecordCollectionTest {
 
     @Test
     void testConvertJaxbToDoma_cpd(){
-        final HashMap<String, String> testInfoJpg = new HashMap<>();
-        final String testTitleJpg = "Foo";
-        testInfoJpg.put("title", testTitleJpg);
-        testInfoJpg.put("fileName", "Foo.jpg");
-        testInfoJpg.put("artist", "Bar");
-        testInfoJpg.put("subject_LCSH", "Fool");
-        testInfoJpg.put("date_Made", "Barl");
-
-        final HashMap<String, String> testInfoCpd = new HashMap<>();
-        final String testTitleCpd = "Fool";
-        testInfoCpd.put("title", testTitleCpd);
-        testInfoCpd.put("fileName", "Fool.cpd");
-        testInfoCpd.put("artist", "Bar");
-        testInfoCpd.put("subject_LCSH", "Fool");
-        testInfoCpd.put("date_Made", "Barl");
-
-        final HashMap<String, String> testInfo = new HashMap<>();
-        final String testTitle = "";
-        testInfo.put("title", testTitle);
-        testInfo.put("fileName", "Fool.jpg");
-        testInfo.put("artist", "Bar");
-        testInfo.put("subject_LCSH", "Fool");
-        testInfo.put("date_Made", "Barl");
-
-        ArtifactRecord recordJpg = ArtifactRecord.withSpecificInfo(testInfoJpg);
-        ArtifactRecord recordCpd = ArtifactRecord.withSpecificInfo(testInfoCpd);
-        ArtifactRecord record = ArtifactRecord.withSpecificInfo(testInfo);
+        ArtifactRecord recordJpg = buildTestRecordJpg();
+        ArtifactRecord recordCpd = buildTestRecordCpd();
+        ArtifactRecord record = buildTestRecordNoTitle();
         JaxbArtifactRecordCollection jaxbCollection = JaxbArtifactRecordCollection.of(recordJpg, record, recordCpd);
         DomaArtifactRecordCollection collection = new DomaArtifactRecordCollection();
         DomaArtifactRecordCollection convertedCollection = collection.convertJaxbToDoma(jaxbCollection);

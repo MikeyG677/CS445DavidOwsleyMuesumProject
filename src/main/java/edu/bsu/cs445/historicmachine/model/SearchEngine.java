d6463c9338aba53ea1012263b3e9c75b67d1ae3a
Jaden <theamericanyank@gmail.com>
2019-04-24 22:04:03 -0400

    Radio Button Functions

--- src/main/java/edu/bsu/cs445/historicmachine/model/SearchEngine.java
+++ src/main/java/edu/bsu/cs445/historicmachine/model/SearchEngine.java
@@ -32,13 +32,32 @@ public class SearchEngine {
     public List<DomaArtifactRecord> searchRelatedWorks(DomaArtifactRecord record){
         LinkedHashSet<DomaArtifactRecord> resultsList = new LinkedHashSet<>();
 
+        resultsList.addAll(searchRelatedWorksByCulture(record));
+        resultsList.addAll(searchRelatedWorksByPeriodStyle(record));
+        resultsList.addAll(searchRelatedWorksByCentury(record));
+
+        System.out.println(searchRelatedWorksByCulture(record));
+        System.out.println(searchRelatedWorksByPeriodStyle(record));
+        System.out.println(searchRelatedWorksByCentury(record));
+
+        return ImmutableList.copyOf(resultsList);
+    }
+
+    public List<DomaArtifactRecord> searchRelatedWorksByCulture(DomaArtifactRecord record){
+        LinkedHashSet<DomaArtifactRecord> resultsList = new LinkedHashSet<>();
+
         String[] culturesSeparated = record.getCulture().split(" ; ", -1);
         for (String cultureSingular : culturesSeparated){
             results = artifacts.stream()
-                    .filter(artifactRecord -> artifactRecord.getCentury().toLowerCase()
+                    .filter(artifactRecord -> artifactRecord.getCulture().toLowerCase()
                             .contains(cultureSingular.toLowerCase())).collect(Collectors.toList());
             resultsList.addAll(results);
         }
+        return ImmutableList.copyOf(resultsList);
+    }
+
+    public List<DomaArtifactRecord> searchRelatedWorksByPeriodStyle(DomaArtifactRecord record){
+        LinkedHashSet<DomaArtifactRecord> resultsList = new LinkedHashSet<>();
 
         String[] periodStylesSeparated = record.getPeriodStyle().split(" ; ", -1);
         for (String periodStyleSingular : periodStylesSeparated){
@@ -47,7 +66,19 @@ public class SearchEngine {
                             .contains(periodStyleSingular.toLowerCase())).collect(Collectors.toList());
             resultsList.addAll(results);
         }
+        return ImmutableList.copyOf(resultsList);
+    }
 
+    public List<DomaArtifactRecord> searchRelatedWorksByCentury(DomaArtifactRecord record){
+        LinkedHashSet<DomaArtifactRecord> resultsList = new LinkedHashSet<>();
+
+        String[] centurySeparated = record.getCentury().split(" ; ", -1);
+        for (String centurySingular : centurySeparated){
+            results = artifacts.stream()
+                    .filter(artifactRecord -> artifactRecord.getCentury().toLowerCase()
+                            .contains(centurySingular.toLowerCase())).collect(Collectors.toList());
+            resultsList.addAll(results);
+        }
         return ImmutableList.copyOf(resultsList);
     }
 }
